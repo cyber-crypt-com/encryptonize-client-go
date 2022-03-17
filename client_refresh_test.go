38ebd9e8dd887f77922aefafe9489b2fc3f1b8ea
Philip Vejre <philip@cyber-crypt.com>
2022-03-17 14:17:03 +0100

    Cleanup error checking in tests

--- client_refresh_test.go
+++ client_refresh_test.go
@@ -23,79 +23,53 @@ import (
 
 func TestUtilityWR(t *testing.T) {
 	c, err := NewClientWR(context.Background(), endpoint, certPath, uid, password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClientWR failed", err, t)
 	defer c.Close()
 
-	if _, err := c.Health(); err != nil {
-		t.Fatal(err)
-	}
-	if _, err := c.Version(); err != nil {
-		t.Fatal(err)
-	}
+	_, err = c.Health()
+	failOnError("Health failed", err, t)
+	_, err = c.Version()
+	failOnError("Version failed", err, t)
 }
 
 func TestUserManagementWR(t *testing.T) {
 	c, err := NewClientWR(context.Background(), endpoint, certPath, uid, password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClientWR failed", err, t)
 	defer c.Close()
 
 	createUserResponse, err := c.CreateUser(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateUser failed", err, t)
 
 	createGroupResponse, err := c.CreateGroup(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateGroup failed", err, t)
 
 	err = c.AddUserToGroup(createUserResponse.UserID, createGroupResponse.GroupID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("AddUserToGroup failed", err, t)
 
 	err = c.RemoveUserFromGroup(createUserResponse.UserID, createGroupResponse.GroupID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("RemoveUserFromGroup failed", err, t)
 
 	err = c.RemoveUser(createUserResponse.UserID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("RemoveUser failed", err, t)
 }
 
 func TestEncryptWR(t *testing.T) {
 	c, err := NewClientWR(context.Background(), endpoint, certPath, uid, password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClientWR failed", err, t)
 	defer c.Close()
 
 	createUserResponse, err := c.CreateUser(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateUser failed", err, t)
 	err = c.LoginUser(createUserResponse.UserID, createUserResponse.Password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("LoginUser failed", err, t)
 
 	plaintext := []byte("foo")
 	associatedData := []byte("bar")
 	encryptResponse, err := c.Encrypt(plaintext, associatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Encrypt failed", err, t)
 
 	decryptResponse, err := c.Decrypt(encryptResponse.ObjectID, encryptResponse.Ciphertext, encryptResponse.AssociatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Decrypt failed", err, t)
 	if string(decryptResponse.Plaintext) != string(plaintext) {
 		t.Fatal("Decryption returned wrong plaintext")
 	}
@@ -106,31 +80,21 @@ func TestEncryptWR(t *testing.T) {
 
 func TestStoreWR(t *testing.T) {
 	c, err := NewClientWR(context.Background(), endpoint, certPath, uid, password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClientWR failed", err, t)
 	defer c.Close()
 
 	createUserResponse, err := c.CreateUser(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateUser failed", err, t)
 	err = c.LoginUser(createUserResponse.UserID, createUserResponse.Password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("LoginUser failed", err, t)
 
 	plaintext := []byte("foo")
 	associatedData := []byte("bar")
 	storeResponse, err := c.Store(plaintext, associatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Store failed", err, t)
 
 	retrieveResponse, err := c.Retrieve(storeResponse.ObjectID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Retrieve failed", err, t)
 	if string(retrieveResponse.Plaintext) != string(plaintext) {
 		t.Fatal("Decryption returned wrong plaintext")
 	}
@@ -141,70 +105,46 @@ func TestStoreWR(t *testing.T) {
 	plaintext = []byte("foobar")
 	associatedData = []byte("barbaz")
 	err = c.Update(storeResponse.ObjectID, plaintext, associatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Update failed", err, t)
 
 	err = c.Delete(storeResponse.ObjectID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Delete failed", err, t)
 }
 
 func TestPermissionsWR(t *testing.T) {
 	c, err := NewClientWR(context.Background(), endpoint, certPath, uid, password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClientWR failed", err, t)
 	defer c.Close()
 
 	createUserResponse, err := c.CreateUser(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateUser failed", err, t)
 	err = c.LoginUser(createUserResponse.UserID, createUserResponse.Password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("LoginUser failed", err, t)
 
 	plaintext := []byte("foo")
 	associatedData := []byte("bar")
 	storeResponse, err := c.Store(plaintext, associatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Store failed", err, t)
 
 	err = c.AddPermission(storeResponse.ObjectID, createUserResponse.UserID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("AddPermission failed", err, t)
 
 	_, err = c.GetPermissions(storeResponse.ObjectID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("GetPermissions failed", err, t)
 
 	err = c.RemovePermission(storeResponse.ObjectID, createUserResponse.UserID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("RemovePermission failed", err, t)
 }
 
 func TestTokenRefreshWR(t *testing.T) {
 	c, err := NewClientWR(context.Background(), endpoint, certPath, uid, password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClientWR failed", err, t)
 	defer c.Close()
 
 	createUserResponse, err := c.CreateUser(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateUser failed", err, t)
 	err = c.LoginUser(createUserResponse.UserID, createUserResponse.Password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("LoginUser failed", err, t)
 
 	// Make sure logic refreshing token is triggered and clear token
 	// to see error if token is not refreshed
@@ -214,7 +154,5 @@ func TestTokenRefreshWR(t *testing.T) {
 	plaintext := []byte("foo")
 	associatedData := []byte("bar")
 	_, err = c.Store(plaintext, associatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Store failed", err, t)
 }
--- client_test.go
+++ client_test.go
@@ -23,6 +23,12 @@ var scopes = []Scope{
 	ScopeUserManagement,
 }
 
+func failOnError(message string, err error, t *testing.T) {
+	if err != nil {
+		t.Fatalf("%s: %v", message, err)
+	}
+}
+
 func TestMain(m *testing.M) {
 	var ok bool
 	uid, ok = os.LookupEnv("E2E_TEST_UID")
@@ -47,88 +53,61 @@ func TestMain(m *testing.M) {
 
 func TestUtility(t *testing.T) {
 	c, err := NewClient(context.Background(), endpoint, certPath)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClient failed", err, t)
 	defer c.Close()
-	if err := c.LoginUser(uid, password); err != nil {
-		t.Fatal(err)
-	}
 
-	if _, err := c.Health(); err != nil {
-		t.Fatal(err)
-	}
-	if _, err := c.Version(); err != nil {
-		t.Fatal(err)
-	}
+	err = c.LoginUser(uid, password)
+	failOnError("LoginUser failed", err, t)
+	_, err = c.Health()
+	failOnError("Health check failed", err, t)
+	_, err = c.Version()
+	failOnError("Version request failed", err, t)
 }
 
 func TestUserManagement(t *testing.T) {
 	c, err := NewClient(context.Background(), endpoint, certPath)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClient failed", err, t)
 	defer c.Close()
-	if err := c.LoginUser(uid, password); err != nil {
-		t.Fatal(err)
-	}
+
+	err = c.LoginUser(uid, password)
+	failOnError("LoginUser failed", err, t)
 
 	createUserResponse, err := c.CreateUser(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateUser failed", err, t)
 
 	createGroupResponse, err := c.CreateGroup(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateGroup failed", err, t)
 
 	err = c.AddUserToGroup(createUserResponse.UserID, createGroupResponse.GroupID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("AddUserToGroup failed", err, t)
 
 	err = c.RemoveUserFromGroup(createUserResponse.UserID, createGroupResponse.GroupID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("RemoveUserFromGroup failed", err, t)
 
 	err = c.RemoveUser(createUserResponse.UserID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("RemoveUser failed", err, t)
 }
 
 func TestEncrypt(t *testing.T) {
 	c, err := NewClient(context.Background(), endpoint, certPath)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClient failed", err, t)
 	defer c.Close()
-	if err := c.LoginUser(uid, password); err != nil {
-		t.Fatal(err)
-	}
+
+	err = c.LoginUser(uid, password)
+	failOnError("LoginUser failed", err, t)
 
 	createUserResponse, err := c.CreateUser(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateUser failed", err, t)
 	err = c.LoginUser(createUserResponse.UserID, createUserResponse.Password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("LoginUser failed", err, t)
 
 	plaintext := []byte("foo")
 	associatedData := []byte("bar")
 	encryptResponse, err := c.Encrypt(plaintext, associatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Encrypt failed", err, t)
 
 	decryptResponse, err := c.Decrypt(encryptResponse.ObjectID, encryptResponse.Ciphertext, encryptResponse.AssociatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Decrypt failed", err, t)
 	if string(decryptResponse.Plaintext) != string(plaintext) {
 		t.Fatal("Decryption returned wrong plaintext")
 	}
@@ -139,34 +118,24 @@ func TestEncrypt(t *testing.T) {
 
 func TestStore(t *testing.T) {
 	c, err := NewClient(context.Background(), endpoint, certPath)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClient failed", err, t)
 	defer c.Close()
-	if err := c.LoginUser(uid, password); err != nil {
-		t.Fatal(err)
-	}
+
+	err = c.LoginUser(uid, password)
+	failOnError("", err, t)
 
 	createUserResponse, err := c.CreateUser(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateUser failed", err, t)
 	err = c.LoginUser(createUserResponse.UserID, createUserResponse.Password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("LoginUser failed", err, t)
 
 	plaintext := []byte("foo")
 	associatedData := []byte("bar")
 	storeResponse, err := c.Store(plaintext, associatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Store failed", err, t)
 
 	retrieveResponse, err := c.Retrieve(storeResponse.ObjectID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Retrieve failed", err, t)
 	if string(retrieveResponse.Plaintext) != string(plaintext) {
 		t.Fatal("Decryption returned wrong plaintext")
 	}
@@ -177,54 +146,36 @@ func TestStore(t *testing.T) {
 	plaintext = []byte("foobar")
 	associatedData = []byte("barbaz")
 	err = c.Update(storeResponse.ObjectID, plaintext, associatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Update failed", err, t)
 
 	err = c.Delete(storeResponse.ObjectID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Delete failed", err, t)
 }
 
 func TestPermissions(t *testing.T) {
 	c, err := NewClient(context.Background(), endpoint, certPath)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("NewClient failed", err, t)
 	defer c.Close()
-	if err := c.LoginUser(uid, password); err != nil {
-		t.Fatal(err)
-	}
+
+	err = c.LoginUser(uid, password)
+	failOnError("LoginUser failed", err, t)
 
 	createUserResponse, err := c.CreateUser(scopes)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("CreateUser failed", err, t)
 	err = c.LoginUser(createUserResponse.UserID, createUserResponse.Password)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("LoginUser failed", err, t)
 
 	plaintext := []byte("foo")
 	associatedData := []byte("bar")
 	storeResponse, err := c.Store(plaintext, associatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("Store failed", err, t)
 
 	err = c.AddPermission(storeResponse.ObjectID, createUserResponse.UserID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("AddPermission failed", err, t)
 
 	_, err = c.GetPermissions(storeResponse.ObjectID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("GetPermissions failed", err, t)
 
 	err = c.RemovePermission(storeResponse.ObjectID, createUserResponse.UserID)
-	if err != nil {
-		t.Fatal(err)
-	}
+	failOnError("RemovePermission failed", err, t)
 }
